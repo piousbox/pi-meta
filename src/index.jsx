941bede7b7fb5c72bc3f388ae3b7f0a52a766a0d
v p <piousbox@vs-MacBook-Pro.local>
2018-05-20 13:28:10 -0700

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Meta extends React.Component {
+  render () {
+    console.log('+++ Meta render')
+    return (
+      <div>meta line</div>
+    )
+  }
+}
+
+export default Meta
+
